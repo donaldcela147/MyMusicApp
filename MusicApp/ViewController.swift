0342a5511b0f6cc8657e07c805336ad29b52083e
Donald <donaldcela.fti@gmail.com>
2020-10-16 10:06:56 +0200

    Initial Commit

---
+++ MusicApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicApp
+//
+//  Created by Florian Cela on 16.10.20.
+//  Copyright © 2020 Florian Cela. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
